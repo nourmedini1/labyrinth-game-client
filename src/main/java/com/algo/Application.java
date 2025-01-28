7fc08fa4eadcc9bd4980a4e5cbdb36c26a7c293f
Karim-zitouna-01 <karimzitouna01@gmail.com>
2025-01-29 00:11:49 +0100

    Add "Color" class for improved terminal output formatting

--- src/main/java/com/algo/Application.java
+++ src/main/java/com/algo/Application.java
@@ -10,9 +10,7 @@ public class Application {
     public static void main(String[] args) {
         displayLogo();
         Player player= new Player();
-        while(player.getName()==null){
-            WelcomePlayer(player);
+        WelcomePlayer(player);
 
-        }
     }
 }
